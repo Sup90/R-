6e38c168cd9b5847c6d7a33fac5ee387fff77201
Sup90 <rhkdtjqql@naver.com>
2018-05-10 02:15:11 +0900

    chapter08

--- bussiness_prac/비즈니스활용사례R08.R
+++ bussiness_prac/비즈니스활용사례R08.R
@@ -2,7 +2,8 @@
 #유저 세그멘테이션 분석려
 s<-getwd()
 s
-setwd("R-/bussiness_prac/R/")
+
+setwd("c:/Users/rhkdt/Desktop/R-/bussiness_prac/R/")
 act<-read.csv("sample-data/section8/daily/action/game-01/2013-10-31/data.tsv",header = T,stringsAsFactors = F,sep="\t")
 head(act)
 dau<-read.csv("sample-data/section8/daily/dau/game-01/")
@@ -78,3 +79,95 @@ head(mau)
 install.packages("ykmeans")
 library(ykmeans)
 mau$payment
+library("ggplot2")
+install.packages("ggplot2")
+library(scales)
+head(user.action)
+user.action2<-ykmeans(user.action,"A47","A47",3)
+#?ykmeans
+#A47이 랭킹이다. 
+a<-kmeans(user.action[,-c(1:3)],centers = 3)
+head(a)
+a$cluster
+user.action$cluster<-a$cluster
+train.user.action<-scale(user.action[])
+user.action[complete.cases(user.action)==T,]
+user.action[]
+head(user.action2)
+table(user.action2$cluster)
+table(user.action$cluster)
+user.action[user.action$cluster==user.action2$cluster,]
+
+ggplot(arrange(user.action2,desc(A47)),
+       aes(x=1:length(user_id),y=A47,
+           #x 축은 그냥 숫자 명수
+           #y 축은 랭킹 포인트 크기순으로 정렬된 값
+           col=as.factor(cluster),
+           shape=as.factor(cluster)))+
+  geom_line()+
+  xlab("user")+
+  ylab("Ranking point")+
+  scale_y_continuous(labels = comma)+
+  ggtitle("Ranking Point")+
+  theme(legend.position = "none")
+  #범례 생략
+
+?ggplot
+
+#상위 랭킹 유저 픽
+user.action.h<-user.action2[user.action2$cluster>=2,names(user.action)]
+
+user.action.h2<-user.action2[user.action2$cluster>=2,]
+length(names(user.action2))
+user.action.h==user.action.h2
+length(names(user.action))
+#전처리를 위해 기계학습 라이브러리 사용
+install.packages("caret")
+library(caret)
+
+user.action.f<-user.action.h[,-c(1:4)]
+user.action.h[,-c(1:4)]
+row.names(user.action.f)<-user.action.h$user_id
+head(user.action.f)
+#정보량이 0에 가까운 변수 제거
+nzv<-nearZeroVar(user.action.f)
+#nearZeroVar
+#분산이 낮은 변수를 제거
+?nearZeroVar
+nzv
+user.action.f.filtered<-user.action.f[,-nzv]
+#변수 간에 상관이 높은 것을 제거
+user.action.cor<-cor(user.action.f.filtered)
+user.action.cor
+highly.cor.f<-findCorrelation(user.action.cor,cutoff = .7)
+#findCorrelation
+#제거해야하는 높은 상관 관계 변수 리스트 반환
+user.action.f.filtered<-user.action.f.filtered[,-highly.cor.f]
+#주성분 분석 실행
+#pca
+user.action.pca.base<-prcomp(user.action.f.filtered,scale=T)
+#scale로 표준화함
+?prcomp
+user.action.pca.base$rotation
+user.action.pca.base$x
+str(user.action.pca.base)
+user.action.pca<-data.frame(user.action.pca.base$x)
+#유저별 주성분 변수 데이터 프레임 삽입
+keys<-names(user.action.pca)
+keys
+user.action.km<-ykmeans(user.action.pca,keys,"PC1",3:6)
+user.action.pca
+user.action.kmㅅ
+table(user.action.km$cluster)
+ggplot(user.action.km,
+       aes(x=PC1,y=PC2,col=as.factor(cluster),shape=as.factor(cluster)))+
+  geom_point()
+user.action.f.filtered$cluster<-user.action.km$cluster
+user.action.f.center<-ldply(lapply(sort(unique(user.action.f.filtered$cluster)),
+  function(i){
+    x<-user.action.f.filtered[user.action.f.filtered$cluster==i,
+                              -ncol(user.action.f.filtered)]
+    apply(x,2,function(d) mean(d))
+      }))
+install.packages("fmsb")
+library("fmsb")
